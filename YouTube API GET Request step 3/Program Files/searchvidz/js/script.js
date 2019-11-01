ea2c2b5f11c6ed732f5469036f221fc01b588255
Karan <karan2711@outlook.com>
2019-11-01 14:08:52 +0530

    All files are included here step by step but when youtbe api needs to be added u need to take your own api while using that

---
+++ YouTube API GET Request step 3/Program Files/searchvidz/js/script.js
@@ -0,0 +1,65 @@
+// Searchbar Handler
+$(function(){
+	var searchField = $('#query');
+	var icon = $('#search-btn');
+	
+	// Focus Event Handler
+	$(searchField).on('focus', function(){
+		$(this).animate({
+			width:'100%'
+		},400);
+		$(icon).animate({
+			right: '10px'
+		}, 400);
+	});
+	
+	// Blur Event Handler
+	$(searchField).on('blur', function(){
+		if(searchField.val() == ''){
+			$(searchField).animate({
+				width:'45%'
+			},400, function(){});
+			$(icon).animate({
+				right:'360px'
+			},400, function(){});
+		}
+	});
+	
+	$('#search-form').submit(function(e){
+		e.preventDefault();
+	});
+})
+
+
+function search(){
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// Get Form Input
+	q = $('#query').val();
+	
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			type:'video',
+			key: 'AIzaSyCvk3NNMQASZgFkCNxIp9jH-l8O0PXhDUo'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				
+				// Log Data
+				console.log(data);
+				
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+					
+					// Display Results
+					$('#results').append(output);
+				});
+			}
+	);
+}
